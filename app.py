b998c4c0c53587fb6d34153aa7f62cf173abcfb0
Rachel Appenzeller <appenzel@usc.edu>
2024-03-03 01:10:27 -0800

    upload file working

--- app.py
+++ app.py
@@ -3,25 +3,34 @@ import requests
 import json
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
 
 API_TOKEN = "US_LOl-j5qoeGNOPOb4Et54RF6Ysc3i_COv1RP9m"
 ACCOUNT_ID = "e400b2d016c3520024ca92809e6e9f4d"
 
 class UploadForm(FlaskForm):
     file = FileField("File")
-    submit = SubmitField("Submit")    
+    submit = SubmitField("Upload File")    
 
 
 @app.route("/")
 def home():    
     return render_template("website_main.html")
 
-@app.route("/record-button")
+@app.route("/record-button", methods=["GET", "POST"])
 def record_button():
-    return render_template("InsertFile_Page.html")
+    form = UploadForm()
+    if form.validate_on_submit():
+        #file = form.file.data
+        #filename = secure_filename(file.filename)
+        #file.save(os.path.join("static", filename))
+        return "File Uploaded"
+    return render_template("InsertFile_Page.html", form = form)
 
 
 def get_text():
